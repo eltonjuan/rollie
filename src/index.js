cfdc8bb07d91f6ed040932a57402063442266c8d
Michael Amos <michael.amos2@nike.com>
2015-11-18 07:01:33 -0800

    some progress

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import {rollbarSnippet, defaultConfig} from './rollbar-snippet';
+
+/**
+ * Rollbar Dust Helper
+ *
+ * Usage: In your page's JavaScript, reference this helper: 
+ *   import 'rolly'
+ *
+ * In your page's Dust template, you can now reference this helper: 
+ * 
+ * {@rollbar accessToken='YOUR_ROLLBAR_ACCESS_TOKEN' }
+ */
+
+if (dust) {
+	dust.helpers.rollbar = function(chunk, context, bodies, params) {
+		const conf = ''// context.resolve(params.rollbarConfig) || defaultConfig
+		console.log(context);
+		const head = `
+			<script>
+				var _rollbarConfig = ${conf};
+				${rollbarSnippet}
+			</script>
+		`
+
+		return chunk.write(head);
+	}
+}
