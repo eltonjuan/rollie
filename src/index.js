c597afd7b9c55d984d008d2d2665882ccdb7546d
Michael Amos <michael.amos2@nike.com>
2015-11-18 22:35:38 -0800

    tabs to spaces

--- src/index.js
+++ src/index.js
@@ -12,36 +12,36 @@ import path from 'path';
  */
 
 if (dust) {
-	dust.helpers.rollbar = function(chunk, context, bodies, params) {
-		let conf;
-		const baseDir = path.join(path.dirname(require.main.filename));
-		if (context.get('rollbarConfig')) {
-			// we have a rollbar config in our context, attempt to parse it
-			try {
-				conf = JSON.stringify(context.get('rollbarConfig'));
-			}	catch (e) {
-				console.error('Error parsing rollbarConfig from Dust context');
-				throw e;
-			}
-		} else if (params.configPath) {
-			// we have a path from which we will attempt to parse the rollbar config
-			try {
-				conf = JSON.stringify(require(path.join(baseDir, params.configPath)));
-			} catch (e) {
-				console.error('Error reading Rollbar config from filesystem')
-				throw e;
-			}
-		} else {
-			throw new Error('No Rollbar config was provided to rollie');
-			return;
-		}
+  dust.helpers.rollbar = function(chunk, context, bodies, params) {
+    let conf;
+    const baseDir = path.join(path.dirname(require.main.filename));
+    if (context.get('rollbarConfig')) {
+      // we have a rollbar config in our context, attempt to parse it
+      try {
+        conf = JSON.stringify(context.get('rollbarConfig'));
+      } catch (e) {
+        console.error('Error parsing rollbarConfig from Dust context');
+        throw e;
+      }
+    } else if (params.configPath) {
+      // we have a path from which we will attempt to parse the rollbar config
+      try {
+        conf = JSON.stringify(require(path.join(baseDir, params.configPath)));
+      } catch (e) {
+        console.error('Error reading Rollbar config from filesystem')
+        throw e;
+      }
+    } else {
+      throw new Error('No Rollbar config was provided to rollie');
+      return;
+    }
 
-		const head = `
-			<script>
-				var _rollbarConfig = ${conf};
-				${rollbarSnippet}
-			</script>
-		`
-		return chunk.write(head);
-	}
+    const head = `
+      <script>
+        var _rollbarConfig = ${conf};
+        ${rollbarSnippet}
+      </script>
+    `
+    return chunk.write(head);
+  }
 }
